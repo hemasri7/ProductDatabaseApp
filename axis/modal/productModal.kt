839d28b4d9f7e072f35f0973c408c18ab2184033
hemasri7 <100147870+hemasri7@users.noreply.github.com>
2022-11-28 09:40:22 +0530

    Delete productModal.kt

--- axis/modal/productModal.kt
+++
@@ -1,8 +0,0 @@
-package com.axis.modal
-
-class Employee(id:Int,name:String,salary:Int,dept:String){
-    var id:Int=id
-    var name:String=name
-    var salary:Int=salary
-    var department:String=dept
-}
